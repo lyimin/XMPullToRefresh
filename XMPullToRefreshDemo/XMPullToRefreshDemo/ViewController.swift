03c4e2f4307ae603b5f603eceb074515ff15bbee
liangyiming <1142343535@qq.com>
2015-10-03 20:47:48 +0800

    add files

---
+++ XMPullToRefreshDemo/XMPullToRefreshDemo/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  XMPullToRefreshDemo
+//
+//  Created by 梁亦明 on 15/10/3.
+//  Copyright © 2015年 xiaoming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+ 
+    var count : Int = 20
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        self.tableView.headerViewPullToRefresh ({ () -> Void in
+            self.getData()
+            self.tableView.reloadData()
+        })
+        
+        self.tableView.footerViewPullToRefresh ({ () -> Void in
+            self.loadMore()
+            self.tableView.reloadData()
+        })
+        
+        self.tableView.headerViewBeginRefreshing()
+    }
+    
+    func getData() {
+        self.count = 20
+    }
+    
+    func loadMore () {
+        self.count += 5
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return count;
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "CellId")
+        cell.textLabel?.text = "XMRefresh : \(indexPath.row)"
+        return cell
+    }
+}
+
